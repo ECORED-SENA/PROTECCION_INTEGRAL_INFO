11c47eef9dc3debfa98251ee15231a3c660b4e6b
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2021-04-27 12:17:58 -0500

    Se realiza el cargue de textos.

---
+++ src/views/Inicio.vue
@@ -0,0 +1,158 @@
+<template lang="pug">
+section
+  BannerPrincipal.mb-5
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 1. Presentación
+
+    p.mb-3 Bienvenido.
+
+    p.mb-3 El curso Aplicación de la estrategia para la protección integral de derechos de niñas, niños y adolescentes, es una iniciativa innovadora liderada por Prosperidad Social con el apoyo del Instituto Colombiano de Bienestar Familiar, el Ministerio de Educación y el Sistema Nacional de Bienestar Familiar, que busca fortalecer la permanencia escolar y la protección de la infancia y la adolescencia en el marco del programa de Familias en Acción. 
+
+    p.mb-3 Aquí no solo se abordan temáticas sumamente relevantes como la superación de la pobreza o la deserción escolar, sino que también se podrán adquirir habilidades para la identificación de estrategias a aplicar en casos puntuales para la solución de diversas problemáticas que afectan la vida de niñas, niños y adolescentes en nuestro país. 
+
+    p.mb-5 Este programa de formación se conecta directamente con la promoción de los derechos de niñas, niños y adolescentes y aborda componentes fundamentales en el trabajo articulado que realizan estas entidades, los cuales son:
+
+    ol.lista-ol.mb-5
+      li 
+        span.text-bold 1. 
+        | Estrategia de alertas para la permanencia escolar y la protección integral de derechos de niñas, niños y adolescentes de Familias en Acción.
+      li 
+        span.text-bold 2. 
+        | Promoción de derechos y prevención de riesgos que afectan a las niñas, niños y adolescentes.
+      li 
+        span.text-bold 3. 
+        | Rutas institucionales de protección de derechos.
+      li 
+        span.text-bold 4. 
+        | Estrategias de permanencia escolar.
+
+    p.mb-3 Cada uno de estos componentes ofrece un marco general de definiciones, conceptos y descripciones específicas de rutas de atención y roles de las entidades encargadas de cada temática. 
+
+    p.mb-3 En ese sentido, este curso se plantea como una alternativa de formación relevante para enlaces municipales de Familias en Acción, rectores, docentes, psicorientadores de colegios, líderes y titulares de Familias en Acción, profesionales de las diferentes instituciones que estén asociados a la Ruta de Alertas a nivel nacional, departamental y municipal y comisarios de familia.
+
+    p Y tiene como finalidad que se consoliden como líderes de cambio en sus comunidades frente a la protección de los derechos, la superación de la pobreza y la permanencia escolar de las niñas, niños y adolescentes.
+
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 2. Justificación<br>del programa
+
+    p.mb-3 La ruta de alertas es una apuesta de articulación entre el ICBF, el Ministerio de Educación, Prosperidad Social y el SNBF que busca fortalecer la permanencia escolar y la protección integral de los niños, niñas y adolescentes participantes de Familias en Acción. Cuenta con una mesa nacional conformada por las direcciones de protección y niñez y adolescencia del ICBF, los grupos internos de trabajo de territorios y poblaciones pilotaje y escalamiento de la dirección de transferencias monetarias condicionadas de Prosperidad Social y la dirección de permanencia del Ministerio de Educación Nacional. En esta mesa se definen los municipios en los que se intervendrá  y las acciones conjuntas que se implementarán para el cumplimiento de los propósitos de la ruta de alerta. Así mismo, se busca integrar las regionales del ICBF y Prosperidad Social con las secretarías de educación departamentales; y en los municipios la alcaldía municipal a través del enlace de Familias en Acción y la Secretaría de Educación Municipal, los rectores de las instituciones educativas y los comisarios de familia.  
+
+    p.mb-3 Esta ruta se desarrolló desde finales del año 2017 en 60 municipios que fueron seleccionados a partir de fuentes de información con las cuales se identificaron las variables territoriales que permitieran seleccionar los municipios con mayor riesgo en trabajo infantil y otro tipo de vulneración de derechos. Durante el segundo semestre del 2018, se realizó el seguimiento a la implementación de la Ruta, para determinar la pertinencia de esta, las condiciones que se requieren para lograr los resultados esperados y realizar el escalamiento a municipios adicionales. El seguimiento mostró un impacto positivo de las acciones propuestas para mejorar los incumplimientos dada la articulación de acciones entre diferentes entidades, la promoción de la oferta y la retroalimentación de las acciones que se desarrollan para dar respuesta a los diferentes casos, sin embargo, una de las falencias encontradas fue la falta de formación en temas relacionados con la protección integral de derechos de niños, niñas y adolescentes y la activación oportuna de esta ruta. 
+
+    p Para superar esta dificultad se elabora un programa de formación que busca brindar herramientas conceptuales y metodológicas a los actores locales que participan en la ruta de alertas de Familias en Acción, con el fin sensibilizarlos, informarlos y formarlos en la promoción de los derechos, la prevención de posibles vulneraciones y el restablecimiento de los derechos de los niños, niñas y adolescentes y en las acciones que pueden implementarse para lograr que se garanticen dichos derechos. 
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 3. Población<br>objetivo
+
+    p.mb-3 Este curso está dirigido a toda la población que se encuentre interesada en la promoción de la protección integral de derechos de niñas, niños y adolescentes, la permanencia escolar y la erradicación de la pobreza en las diferentes regiones del país; desde la diversidad de experiencias y conocimientos que pueden aportar los participantes durante todo el proceso. 
+
+    p Asimismo, esta formación es pertinente para todos los actores institucionales y comunitarios que deseen fortalecer su capacidad de liderazgo en la solución de las problemáticas mencionadas anteriormente y en el mejoramiento de la calidad de vida de sus comunidades. 
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 4. Estrategia<br>metodológica
+
+    p.mb-3 Centrada en la construcción de autonomía para garantizar la calidad de la formación en el marco de la formación por competencias, el aprendizaje por proyectos y el uso de técnicas didácticas activas que estimulan el pensamiento para la resolución de problemas simulados y reales; soportadas en la utilización de las tecnologías de la información y la comunicación, integradas, en ambientes virtuales de aprendizaje, que en todo caso recrean el contexto productivo y vinculan al aprendiz con la realidad cotidiana y el desarrollo de las competencias.
+
+    p.mb-3 Igualmente, debe estimular de manera permanente la autocrítica y la reflexión del aprendiz sobre el quehacer y los resultados de aprendizaje que logra a través de la vinculación activa de las cuatro (4) fuentes de información para la construcción de conocimiento:
+
+    ul
+      li 
+        i.lista-ul__vineta
+        | El instructor – Tutor.
+      li 
+        i.lista-ul__vineta
+        | El entorno.
+      li 
+        i.lista-ul__vineta
+        | Las TIC.
+      li 
+        i.lista-ul__vineta
+        | El trabajo colaborativo.
+
+
+  //- Créditos
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 Créditos
+
+    CreditosComp
+
+  Footer
+  
+
+</template>
+<script>
+import { menuPrincipal } from '../config/global'
+import BannerPrincipal from '../components/plantilla/BannerPrincipal'
+import CreditosComp from '../components/plantilla/CreditosComp'
+import Footer from '../components/plantilla/Footer'
+export default {
+  name: 'Inicio',
+  components: {
+    BannerPrincipal,
+    CreditosComp,
+    Footer,
+  },
+  data: () => ({
+    menuPrincipalData: menuPrincipal,
+  }),
+  computed: {
+    desarrolloContenidosData() {
+      const allMenuData = [
+        ...this.menuPrincipalData.menu,
+        ...this.menuPrincipalData.subMenu,
+      ]
+      return allMenuData.filter(item => item.desarrolloContenidos)
+    },
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+.resultados-aprendizaje
+  &__item
+    margin-bottom: 20px
+    &:last-child
+      margin-bottom: 0
+
+.desarrollo-contenidos
+  &__item
+    display: flex
+    align-items: center
+    justify-content: space-between
+    margin-bottom: 25px
+    padding: 5px
+    border-radius: $base-border-radius
+
+    &__texto
+      display: flex
+      align-items: center
+
+    &__icono
+      display: block
+      background-color: $color-sistema-g
+      font-size: 20px
+      font-weight: $base-black-font-weight
+      text-align: center
+      padding: 10px
+      line-height: 1em
+      width: 40px
+      margin-right: 15px
+
+    &:last-child
+      margin-bottom: 0
+
+    &:hover
+      background-color: $color-sistema-g
+      font-weight: $base-black-font-weight
+      color: $color-sistema-a
+</style>
